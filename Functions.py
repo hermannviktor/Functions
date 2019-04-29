53e656ca86f725258f8675ec75aac817233d2433
hermannviktor <hermannv@mit.edu>
2019-04-29 00:14:46 -0400

    test

--- Functions.py
+++ Functions.py
@@ -7,11 +7,12 @@ def test(input):
     '''
     return input * 2
 
-def test2(input)
+def test2(input):
     '''
     :param input:
         - input is number
     :return:
         - return twice amount
     '''
+
     return input * 3
